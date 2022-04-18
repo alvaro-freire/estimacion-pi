1ccc17fe31ac9adb29607095791227321632f2e9
mateo-diaza <mateodiaz780@gmail.com>
2022-04-18 16:44:17 +0200

    Use of Bcast & Reduce

--- practica2.c
+++ practica2.c
@@ -9,11 +9,11 @@ int main(int argc, char *argv[]) {
     double pi, x, y, z;
 
     // variables for MPI
-    int n_proc, rank, j;
+    int n_procs, rank;
     double total_pi;
 
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     // rank 0 -> master
@@ -21,21 +21,17 @@ int main(int argc, char *argv[]) {
         if (rank == 0) {    // master sends data
             printf("Enter the number of points: (0 quits) \n");
             scanf("%d", &n);
-
-            for (j = 1; j < n_proc; ++j) {
-                MPI_Send(&n, 1, MPI_INT, j, 0, MPI_COMM_WORLD);
-            }
             total_pi = 0;
-        } else {    // worker receives data
-            MPI_Recv(&n, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
+
+        MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
         if (n == 0) {
             break;
         }
 
         count = 0;
-        for (i = 1; i <= n; i += n_proc) {
+        for (i = 1; i <= n; i += n_procs) {
             x = ((double) rand()) / ((double) RAND_MAX);
             y = ((double) rand()) / ((double) RAND_MAX);
             z = sqrt((x * x) + (y * y));
@@ -46,16 +42,9 @@ int main(int argc, char *argv[]) {
         }
         pi = ((double) count / (double) n) * 4.0;
 
-        if (rank == 0) {    // master receives results
-            total_pi = pi;
-
-            for (j = 1; j < n_proc; j++) {
-                MPI_Recv(&pi, 1, MPI_DOUBLE, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                total_pi += pi;
-            }
+        MPI_Reduce(&pi, &total_pi, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0) {
             printf("pi is approx. %.16f, Error is %.16f\n", total_pi, fabs(total_pi - PI25DT));
-        } else {    // worker sends results
-            MPI_Send(&pi, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
         }
     }
     MPI_Finalize();
